01fc14f395a1115751dcefe4e49a82ef64280b73
tanmay-ninawe <tanmay.ninawe72@gmail.com>
2026-05-20 09:12:21 +0530

    Day 2 : Problem 6

---
+++ src/Problem6_PrintTwentyToOne.java
@@ -0,0 +1,17 @@
+
+
+public class Problem6_PrintTwentyToOne {
+
+    public static void main (String[] args) {
+
+        int num = 20;
+
+        for(int i = num; i >= 0; i--){
+            System.out.println(i);
+        }
+
+
+    }
+}
+    
+
