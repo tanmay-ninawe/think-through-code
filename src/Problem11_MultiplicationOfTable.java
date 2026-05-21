8a5f29d7f3ad404f72b958c794cc05ffb49d5b69
tanmay-ninawe <tanmay.ninawe72@gmail.com>
2026-05-21 20:10:03 +0530

    Day 3 : program 11

---
+++ src/Problem11_MultiplicationOfTable.java
@@ -0,0 +1,22 @@
+public class Problem11_MultiplicationOfTable {
+
+    public static void main(String[] args) {
+        
+   //Print multiplication table of 7 (7×1=7 up to 7×10=70)
+
+   int digit = 7;
+
+   for(int i=1; i<=10; i++){
+    int res1 = digit*i;
+
+    System.out.println(digit + "x" + i + " = " + res1);
+   } 
+
+    
+
+    
+
+
+    
+}
+}
