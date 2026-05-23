e3f2e42323cf15e2ff90648b245c2935d2151d67
tanmay-ninawe <tanmay.ninawe72@gmail.com>
2026-05-23 19:32:28 +0530

    Day4 : Problem 14

---
+++ src/Problem14_ReverseNumber.java
@@ -0,0 +1,19 @@
+public class Problem14_ReverseNumber {
+
+    public static void main(String[] args) {
+
+        int a = 12345;
+        int rev = 0;
+
+        while (a>0) {
+            rev = rev * 10 + (a %10);
+            a = a /10;
+        } 
+        System.out.println(rev);  
+
+
+        }
+
+    }
+
+
