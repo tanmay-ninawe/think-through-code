ed9b1cd41c8ef50a2ce7f9be8c4e3042dc5cb66d
tanmay-ninawe <tanmay.ninawe72@gmail.com>
2026-05-28 21:40:00 +0530

    Day6 : Problem 17

---
+++ src/Problem17_PrintFactorialOf6.java
@@ -0,0 +1,27 @@
+public class Problem17_PrintFactorialOf6 {
+
+    public static void main(String[] args) {
+
+        // Factorial of 6 (expected: 720)
+
+        int num = 6;
+        int result = 1;
+
+        for (int i = 1; i <= num; i++) {
+
+            result = result * i;
+            System.out.println(result);
+
+        }
+
+        /*
+         * ################# Logic
+         * 
+         * 1. scan for the numbers.
+         * 2. calculate each factorial for that number
+         * 
+         * 
+         */
+
+    }
+}
